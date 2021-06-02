5ff75f64f097011d2410fa6eedf557777efc90d4
alexb31 <alexandrebornstein@gmail.com>
2021-06-02 17:14:57 +0200

    Added Navbar

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -3,17 +3,17 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(() => ({
     root: {
         maxWidth: '100%',
-    },
-    media: {
+      },
+      media: {
         height: 0,
         paddingTop: '56.25%', // 16:9
-    },
-    cardActions: {
+      },
+      cardActions: {
         display: 'flex',
         justifyContent: 'flex-end',
-    },
-    cardContent: {
+      },
+      cardContent: {
         display: 'flex',
         justifyContent: 'space-between',
-    },
+      },
 }));
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,22 +2,27 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', img: 'https://www.benson-shoes.com/Lib/Images/Home/Slide1.jpg'},
     { id: 2, name: 'Laptop', description: 'Asus Rog', price: '$10', img: 'https://www.notebookcheck.biz/uploads/tx_nbc2/csm_GL504_Hero_Cam05Lighting_v1_740f45a0ab.jpg'},
 ]
 
+
 const Products = () => {
+    const classes = useStyles();
     return (
-        <main>
-            <Grid container justify="center" spacing={4}>
-                {products.map((product) => (
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product}/>
-                    </Grid>
-                ))}
-            </Grid>
+        <main className={classes.content}>
+            <div className={classes.toolbar}>
+                <Grid container justify="center" spacing={4}>
+                    {products.map((product) => (
+                        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                            <Product product={product}/>
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>
         </main>
     )
 }
