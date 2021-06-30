889a487d3c5244f0d69e44f93fb4233da70ff12b
alexb31 <alexandrebornstein@gmail.com>
2021-06-30 18:28:20 +0200

    Payment Form & Stripe

--- src/components/CheckoutForm/Review.jsx
+++ src/components/CheckoutForm/Review.jsx
@@ -5,7 +5,7 @@ const Review = ({checkoutToken}) => {
     return (
         <>
             <Typography variant="h6" gutterBottom>Order Summary</Typography>
-            <List disabledPadding>
+            <List disablePadding>
                 {checkoutToken.live.line_items.map((product) => (
                     <ListItem style={{padding: '10px 0'}} key={product.name}>
                         <ListItemText primary={product.name} secondary={`Quantity: ${product.quantity}`}/>
